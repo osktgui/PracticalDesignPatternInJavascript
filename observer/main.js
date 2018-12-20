8ba40610500a5b43c806ff28933d94f7df851cf2
Luis Carlos Oscátegui <luis@xurface.com>
2018-12-19 20:48:37 -0500

    Adding Our Subject

--- observer/main.js
+++ observer/main.js
@@ -35,10 +35,40 @@ ObserverList.prototype.get = function (index) {
   }
 };
 
-var task1 = new Task({ name: 'create a demo for constructors', user: 'Jon' });
+ObserverList.prototype.count = function () {
+  return this.observerList.length;
+};
+
+
+var ObservableTask = function (data) {
+  Task.call(this, data);
+  this.observers = new ObserverList();
+};
+
+ObservableTask.prototype.addObserver = function (observer) {
+  this.observers.add(observer);
+};
+
+ObservableTask.prototype.notify = function (context) {
+  var observerCount = this.observers.count();
+  for (var i = 0; i < observerCount; i++) {
+    this.observers.get(i)(context);
+  }
+};
+
+ObservableTask.prototype.save = function () {
+  this.notify(this);
+  Task.prototype.save.call(this);
+};
+
+var task1 = new ObservableTask({ name: 'create a demo for constructors', user: 'Jon' });
 
 var not = new notificationService();
 var ls = new loggingService();
 var audit = new auditingService();
+
+task1.addObserver(not.update);
+task1.addObserver(ls.update);
+task1.addObserver(audit.update);
 
 task1.save();
