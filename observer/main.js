8b4245b943df74fbc69d77aec20882fc5ba24b6a
Luis Carlos Oscátegui <luis@xurface.com>
2018-12-19 20:34:28 -0500

    Creating Our Observers

--- observer/main.js
+++ observer/main.js
@@ -1,4 +1,30 @@
 var Task = require('./task');
 
+var notificationService = function () {
+  var message = 'Notifying ';
+  this.update = function(task) {
+    console.log(message + task.user + ' for task ' + task.name);
+  };
+};
+
+var loggingService = function () {
+  var message = 'Logging ';
+  this.update = function(task) {
+    console.log(message + task.user + ' for task ' + task.name);
+  };
+};
+
+var auditingService = function () {
+  var message = 'Auditing ';
+  this.update = function(task) {
+    console.log(message + task.user + ' for task ' + task.name);
+  };
+};
+
 var task1 = new Task({ name: 'create a demo for constructors', user: 'Jon' });
+
+var not = new notificationService();
+var ls = new loggingService();
+var audit = new auditingService();
+
 task1.save();
